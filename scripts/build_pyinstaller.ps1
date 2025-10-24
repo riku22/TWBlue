ad50f19e13ca7acd580307752b48131a8f41364c
riku <riku@riku22.net>
2025-10-24 10:35:35 +0900

    Migrate to uv

--- scripts/build_pyinstaller.ps1
+++ scripts/build_pyinstaller.ps1
@@ -2,14 +2,14 @@
 # Must be called from root of repo
 echo "Generating documentation..."
 cd doc
-python documentation_importer.py
-python generator.py
+uv run documentation_importer.py
+uv run generator.py
 mv documentation ..\src
 cd ..
 echo "done."
 
 echo "Building binary..."
 cd src
-python -OO -m PyInstaller main_1.spec
+uv run python -OO -m PyInstaller main_1.spec
 cd ..
 echo "done."
